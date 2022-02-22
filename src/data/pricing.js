b2bf6a7a75149e5105ec61bacf1ec71561111b4d
Chris Pennington <chris@codinginpublic.dev>
2022-02-22 15:43:23 -0700

    Website is done!

---
+++ src/data/pricing.js
@@ -0,0 +1,59 @@
+const pricingData = [
+  {
+    price: "$1000",
+    plan: "Starter Plan",
+    description: "Landing page to maximize conversion rate",
+    details: [
+      "Custom Branding",
+      "Single Landing page",
+      "SEO-optimized",
+      "One Email inbox",
+      "Contact Form",
+    ],
+    button: {
+      text: "Learn more",
+      url: "#",
+      style: "muted",
+    },
+    subtext: "$500 up front + $500 upon completion",
+  },
+  {
+    price: "$1400",
+    plan: "Pro Plan",
+    description: "Personalized site to showcase your brand",
+    details: [
+      "Starter Plan+",
+      "Up to 5 pages",
+      "Social feed integrations",
+      "CMS for custom content",
+      "Custom analytics",
+    ],
+    button: {
+      text: "Sign up",
+      url: "#",
+      style: "secondary",
+    },
+    subtext: "$700 up front + $700 upon completion",
+    featured: true,
+  },
+  {
+    price: "$2000",
+    plan: "Writer’s Plan",
+    description: "Personalized site with accompanying blog",
+    details: [
+      "Pro Plan+",
+      "Full-featured blog",
+      "Search functionality",
+      "Two email inboxes",
+      "Free domain name (1 year)",
+    ],
+    button: {
+      text: "Learn more",
+      url: "#",
+      style: "muted",
+    },
+    subtext: "$1000 up front + $1000 upon completion",
+  },
+];
+
+export default pricingData;
