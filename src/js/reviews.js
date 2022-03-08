963307a31fca407c6f34b9f8b0b17052817fd078
Chris Pennington <chris@codinginpublic.dev>
2022-03-08 06:56:31 -0700

    updated stylesheet link to new astro version and fixed a few more template classes

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -5,62 +5,66 @@ const indicators = [...document.querySelectorAll('.indicator')];
 let isMoving;
 let currentIndex = 1;
 
-function showActiveIndicator(){
-  indicators.forEach(ind => ind.classList.remove('active'));
-  let activeIndicator;
-  if(currentIndex === 0 || currentIndex === reviews.length - 2){
-    activeIndicator = indicators.length - 1;
-  } else if (currentIndex === reviews.length - 1 || currentIndex === 1){
-    activeIndicator = 0;
-  } else {
-    activeIndicator = currentIndex - 1;
-  }
-  indicators[activeIndicator].classList.add('active');
+function showActiveIndicator() {
+    indicators.forEach((ind) => ind.classList.remove('active'));
+    let activeIndicator;
+    if (currentIndex === 0 || currentIndex === reviews.length - 2) {
+        activeIndicator = indicators.length - 1;
+    } else if (currentIndex === reviews.length - 1 || currentIndex === 1) {
+        activeIndicator = 0;
+    } else {
+        activeIndicator = currentIndex - 1;
+    }
+    indicators[activeIndicator].classList.add('active');
 }
 
-function moveSlider(){
-  reviewsSlider.style.transform = `translateX(-${currentIndex * 100}%)`;
-  showActiveIndicator();
-};
-
-function handleReviewBtnClick(e){
-  if(isMoving){ return };
-  isMoving = true;
-  e.currentTarget.id === 'next'
-    ? currentIndex++
-    : currentIndex--;
-  moveSlider();
+function moveSlider() {
+    reviewsSlider.style.transform = `translateX(-${currentIndex * 100}%)`;
+    showActiveIndicator();
 }
 
-function handleIndicatorClick(e){
-  if(isMoving){ return };
-  const clickedIndicator = indicators.indexOf(e.target) + 1;
-  if(clickedIndicator === currentIndex){ return };
-  isMoving = 'true';
-  currentIndex = clickedIndicator;
-  moveSlider();
+function handleReviewBtnClick(e) {
+    if (isMoving) {
+        return;
+    }
+    isMoving = true;
+    e.currentTarget.id === 'next' ? currentIndex++ : currentIndex--;
+    moveSlider();
+}
+
+function handleIndicatorClick(e) {
+    if (isMoving) {
+        return;
+    }
+    const clickedIndicator = indicators.indexOf(e.target) + 1;
+    if (clickedIndicator === currentIndex) {
+        return;
+    }
+    isMoving = 'true';
+    currentIndex = clickedIndicator;
+    moveSlider();
 }
 
 // Event Listeners
-reviewBtns.forEach(btn => {
-  btn.addEventListener('click', handleReviewBtnClick);
-})
+reviewBtns.forEach((btn) => {
+    btn.addEventListener('click', handleReviewBtnClick);
+});
 
-indicators.forEach(ind => {
-  ind.addEventListener('click', handleIndicatorClick);
-})
+indicators.forEach((ind) => {
+    ind.addEventListener('click', handleIndicatorClick);
+});
 
 reviewsSlider.addEventListener('transitionend', () => {
-  isMoving = false;
-  if(currentIndex === 0){
-    currentIndex = reviews.length - 2;
-    reviewsSlider.style.transitionDuration = '1ms';
-    return moveSlider();
-  }
-  if(currentIndex === reviews.length - 1){
-    currentIndex = 1;
-    reviewsSlider.style.transitionDuration = '1ms';
-    return moveSlider();
-  }
-  reviewsSlider.style.transitionDuration = '300ms';
-})
+    isMoving = false;
+    if (currentIndex === 0) {
+        currentIndex = reviews.length - 2;
+        reviewsSlider.style.transitionDuration = '1ms';
+        return moveSlider();
+    }
+    if (currentIndex === reviews.length - 1) {
+        currentIndex = 1;
+        reviewsSlider.style.transitionDuration = '1ms';
+        return moveSlider();
+    }
+    reviewsSlider.style.transitionDuration = '300ms';
+});
